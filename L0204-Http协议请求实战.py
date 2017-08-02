9871654ed8acfdd9115ce171c0b03f6b8d975edb
Fu4ng <903838834@.com>
2017-08-02 14:22:48 +0800

    GetandPost

---
+++ L0204-Http协议请求实战.py
@@ -0,0 +1,34 @@
+# --------------------------------------#
+#
+# Created by Fu4ng on 2017/8/2.
+#
+# 博文地址:
+# --------------------------------------#
+import urllib.request
+import urllib.parse
+
+# GET
+keyword = 'keyword'  # 你想搜索的关键字
+"""
+如果关键字是中文的话，涉及到编码
+keyword = "中文关键字"
+keyword_code = urllib.request.quote(keyword)
+url = "http://baidu.com/s?wd="+keyword_code
+"""
+url = "http://baidu.com/s?wd=" + keyword
+req = urllib.request.Request(url)
+data = urllib.request.urlopen(req).read()
+print(data)
+
+# Post
+url = "http://www.iqianyue.com/mypost"
+postdata = urllib.parse.urlencode({"name": "test", "pass": "lolp"}).encode("utf-8")
+req = urllib.request.Request(url, postdata)
+# 模拟浏览器
+req.add_header(
+    'User-Agent',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36')
+data = urllib.request.urlopen(req).read()
+fhandle = open("post.html", 'wb')
+fhandle.write(data)
+fhandle.close()
